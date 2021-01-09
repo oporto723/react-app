8bdd86df0c46e786629ed08d1f02aa848546c9d1
Jonathan Oporto <jonathan.oporto@gmail.com>
2021-01-09 12:01:46 -0800

    add Navbar using react-bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # react-app
+
+This will be just a simple React app where I will trying different features.
+
+### Install React
+
+npx create-react-app my-app
+cd my-app
+npm start
+
+### Install react-boostrap 
+
+npm install react-bootstrap bootstrap
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown} from "react-bootstrap"
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React Application</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Project 1</Nav.Link>
+      <Nav.Link href="#pricing">Project 2</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action 1</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Action 2</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Action 3</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Action 4</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">Project 3</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
     </div>
   );
 }
